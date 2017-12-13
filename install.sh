0794c7bfe9bebd8afeb96235abad92f9611ae49e
Luiz Gustavo <luizgustavo@w4msolutions.com>
2017-12-13 16:36:00 +0000

    rename file

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd /usr/local/www
-fetch -q -o - https://github.com/lgcosta/db_consultingti/raw/prod/auth_proxy.tar.gz | tar zxpf -
+fetch -q -o - https://github.com/lgcosta/db_consultingti/raw/prod/password_proxy.tar.gz | tar zxpf -
